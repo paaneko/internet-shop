400ad2d667a5cc8051eaf8f9e8fddfab8452f1e6
paaneko <paamayim.n3kudotayim@gmail.com>
2023-12-05 01:08:40 +0200

    seeder:Add data to CharacteristicAttributeSeeder

--- database/seeders/CharacteristicAttributeSeeder.php
+++ database/seeders/CharacteristicAttributeSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Characteristic;
+use App\Models\CharacteristicAttribute;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 
 class CharacteristicAttributeSeeder extends Seeder
@@ -11,6 +14,29 @@ class CharacteristicAttributeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $characteristicsIds = Characteristic::all()->pluck('id')->toArray();
+
+        $faker = Faker::create();
+
+        foreach ($characteristicsIds as $characteristicId) {
+            $countOfCharacteristicAttributes = $faker->optional(
+                $weight = 0.6,
+                $default = 1
+            )->numberBetween(2, 12);
+            for ($x = 0; $x < $countOfCharacteristicAttributes; $x++) {
+                CharacteristicAttribute::create([
+                    'characteristic_id' => $characteristicId,
+                    // TODO Think about making this column unique
+                    // 🪄 Here starting faker magic 🔮🔮🔮
+                    'name' => ucfirst(
+                        $faker->word().' '.$faker->optional(
+                            0.5,
+                            ''
+                        )->words($faker->numberBetween(1, 2), true)
+                    ),
+                    'sorting_order' => $x + 1,
+                ]);
+            }
+        }
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
 
             CharacteristicGroupSeeder::class,
             CharacteristicSeeder::class,
+            CharacteristicAttributeSeeder::class,
         ]);
 
         // \App\Models\User::factory(10)->create();
