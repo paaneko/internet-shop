0f22d4b2913cfcacdd82a7ff13c6429c02dcc81b
paaneko <paamayim.n3kudotayim@gmail.com>
2023-12-25 17:42:37 +0200

    test(resource): Characteristic
    Test covering

--- app/Models/Characteristic.php
+++ app/Models/Characteristic.php
@@ -22,6 +22,7 @@ class Characteristic extends Model
             'name',
             'hint_text',
             'is_collapsed',
+            'sorting_order',
         ];
 
     public function characteristicGroup(): BelongsTo
