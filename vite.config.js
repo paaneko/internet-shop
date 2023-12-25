b9af0239d0c5e5872b48ec8f9564f0de116576b9
paaneko <paamayim.n3kudotayim@gmail.com>
2023-12-25 23:48:13 +0200

    chore(tailwind): Set up and config hot reloading

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
@@ -7,5 +7,14 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+
     ],
+    // Use it if you running app on herd
+    // Otherwise comment this
+    server: {
+        host: 'internet-shop.test',
+        watch: {
+            usePolling: true,
+        },
+    },
 });
