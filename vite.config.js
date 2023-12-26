3050359b6f15843b826d3faf3cc467d981341c0f
paaneko <paamayim.n3kudotayim@gmail.com>
2023-12-26 23:51:56 +0200

    chore(daisyui): Install DaisyUI

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
-import {defineConfig} from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true
+        })
     ],
     // Use it if you running app on herd
     // Otherwise comment this
     server: {
-        host: 'internet-shop.test',
+        host: "internet-shop.test",
         watch: {
-            usePolling: true,
-        },
-    },
+            usePolling: true
+        }
+    }
 });
