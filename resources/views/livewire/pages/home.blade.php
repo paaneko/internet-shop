da0dbb062a1ad8c9dbf9bcb6f36a4878b7d95611
paaneko <paamayim.n3kudotayim@gmail.com>
2024-06-06 00:23:41 +0300

    feat(home-page): Implement brand list component

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -4,6 +4,10 @@
         <x-entities.limited-offers-carousel :$products />
     </div>
     <div>
+        <h2 class="my-5 text-2xl font-semibold">Brand's</h2>
+        <x-entities.brands-list />
+    </div>
+    <div>
         <h2 class="my-5 text-2xl font-semibold">Today's Best Deals</h2>
         <x-entities.product-carousel-list :$products />
     </div>
